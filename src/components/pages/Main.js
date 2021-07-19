aafbd51c0b474bf86b0b58aa4e8874db5662d408
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2021-07-19 21:19:51 +0600

    this commit was made 19.07.2021 main page was modified

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,32 +1,62 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useEffect, Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {MainPageInfoShow} from '../../redux/actions/main_page_info';
+import { GetCategories } from '../../redux/actions/categories';
 
 const Main = ({
     MainPageInfoShow,
-    main_page_info: {main_page_info, loading}
+    main_page_info: {main_page_info, loading},
+    categories: {categories}, GetCategories
 }) => {
 
     useEffect(() => {
         MainPageInfoShow()
-    },[])
+    },[]);
+
+    useEffect(() => {
+        GetCategories()
+    },[GetCategories]);
+    
     return (
-        <div>
+        <div style={{position: "absolute", top: "0%", left: "0%"}}>
             {loading ? (
                 <Fragment>
-                    <div>sdfsdfsdfsdfsgfhfghdfghdfghdfghdfghdfghdfghdf</div>
+                    <h3 className="text-center">Строительная компания "Нурлы Табыс"</h3>
                 </Fragment>
             ) : (
                 <Fragment>
-                    {main_page_info && main_page_info.video.map((v, index) => 
+                    {main_page_info && main_page_info.video.map((v, index) =>
                         <div key={index} className="main-page-video-div">
-                        <video className="main-page-video-video" autoPlay loop muted 
-                        
-                        src={`http://localhost:5003/${v.filePath}`}>     
-                        </video>
-                    </div>
-                )}
+                            <video className="main-page-video-video" autoPlay loop muted 
+                                src={`http://localhost:5003/${v.filePath}`}
+                            >     
+                            </video>
+                            <div className="main-page-content-div-absolute">
+                                <div className="main-page-title">
+                                    <h4 className="text-center p-5">{v.title}</h4>
+                                </div>
+                                <div className="main-page-companyInfo">
+                                    <p>{v.companyInfo}</p>
+                                </div>
+                                <p className="main-page-description">{v.description}</p>
+
+                                <p className="main-page-contacts">{v.contacts}</p> 
+
+                                {categories && categories.map((category, index) =>
+                                    <div className="main-page-categories" key={index}>
+                                        <Link to={`/category/${category.slug}`}>{category.name}</Link>
+                                    </div>
+                                )}
+                            </div>
+
+                    
+                               
+                               
+                 
+                        </div>  
+                    )}
                 </Fragment>
             )}
             
@@ -36,16 +66,24 @@ const Main = ({
 
 Main.propTypes = {
     MainPageInfoShow: PropTypes.func.isRequired,
-    main_page_info: PropTypes.array.isRequired,
+    main_page_info: PropTypes.object.isRequired,
+    GetCategories: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
-    main_page_info: state.main_page_info
+    main_page_info: state.main_page_info,
+    categories: state.categories
 })
 
 export default connect(mapStateToProps, {
-    MainPageInfoShow
+    MainPageInfoShow,
+    GetCategories
 })(Main)
 
 
 
+
+
+// filePath: "uploads/1626422495966_small-mech1.mp4"
+
+// filePath: "uploadsMainPageInfo/1626626365772_Advertizement2.mp4"
