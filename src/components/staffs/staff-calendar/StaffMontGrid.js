961d9f7ab38b0cff5eddb395dee537eaa4ddc328
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2021-09-30 21:19:31 +0600

    some minor changes were added to calendar

--- src/components/staffs/staff-calendar/StaffMontGrid.js
+++ src/components/staffs/staff-calendar/StaffMontGrid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styled from 'styled-components';
 import '../../boss/company-management/tasks/calendar/calendar.css'
 import moment from 'moment';
@@ -19,7 +19,6 @@ const StaffMonthGrid = ({
     const currentDay = (day) => moment().isSame(day, 'day');
     const isSelectedMonth = (day) => moment().isSame(day, 'month');
 
-    let dayOptions = {weekday: 'long'}
     return (
         <div className='calendar'>
             {[...Array(7)].map((_, i) => (
@@ -52,14 +51,19 @@ const StaffMonthGrid = ({
                         <ul className='tasks-list-wrapper'>
                             {tasks_by_role.tasks && tasks_by_role.tasks.filter(task => task.finish.split('T', 1)[0] >= dayItem.format('YYYY-MM-DD') && task.finish.split('T', 1)[0] <= dayItem.clone().endOf('day').format('YYYY-MM-DD'))
                                 .map((task) => 
-                                (
-                                    <li className='' key={task._id}>
-                                        <div className='task-button'>
+                                (<>
+                                    <li className='' key={task._id} >
+                                        <div 
+                                            className='task-button'
+                                            //className={color}    
+                                        >
                                             <Link to={`task-full-info/${task._id}`}>
                                                 {task.title}
                                             </Link>
                                         </div>
                                     </li>
+                                     <br />
+                                     </>
                                 ))
                             }
                         </ul>
