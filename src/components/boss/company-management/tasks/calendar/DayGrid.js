eec604a1edd8d915e8a8151996098a56d52be3ee
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2021-10-18 13:18:37 +0600

    spinner was added to DayGrid

--- src/components/boss/company-management/tasks/calendar/DayGrid.js
+++ src/components/boss/company-management/tasks/calendar/DayGrid.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './calendar.css'
 import { Link } from 'react-router-dom';
+import Spinner from '../../../../layout/Spinner'
 
 
 const DayGrid = ({
@@ -10,45 +11,53 @@ const DayGrid = ({
 }) => {
     
     return (
-        
-        <div className='day-calendar'>
-            <div className='bg-info' onClick={() => openModalHandler('Create')}><p className='text-center text-dark' style={{cursor: 'pointer', fontWeight: 'bold'}}>Дать поручение</p></div>
-            <ul>
-                {all_tasks.tasks && all_tasks.tasks.filter(task => task.finish.split('T', 1)[0] >= startDay.format('YYYY-MM-DD') && task.finish.split('T', 1)[0] <= startDay.clone().endOf('day').format('YYYY-MM-DD'))
-                    .map((task) => 
-                        (
-                            <Link to={`task-full-info/${task._id}`}>
-                                <div className='m-3 bg-warning px-3'>
-                                    {task.content}
-                                    <br />
-                                    <p className="app-text-small d-inline p-3">Выполнить к:</p>
-                                    <p className="d-inline">{new Date(task.finish).toLocaleString('ru').substr(11)}</p>
-                                </div>
-                                <li className='day-tasks-list' key={task._id}>
-                                    <p>Исполнители:</p>
-                                    
-                                    {task && task.employee.map((e) => (
-                                       
-                                        <div key={e._id}>
-                                            <span >{e.name}</span>
-                                            <span className='mx-2'>{e.lastName}</span>
-
-                                        {task && task.read.map((r) => (
-                                            <div className='px-2' key={r._id}>{e._id === r.byEmployee && r.byEmployee !== null && r.ok === true && r.ok !== null ? (<p className='px-2 m-3 bg-info text-dark'>Задание прочитано и принято к исполнению</p>) : null}</div>
-                                        )) } 
-
-                                        {task && task.completed.map((c) => (
-                                            <div className='px-2' key={c._id}>{e._id === c.byEmployee && c.byEmployee !== null && c.done === true && c.done !== null ? (<p className='px-2 m-3 bg-success text-white'>Задание выполнено</p>) : null}</div>
-                                        )) }
-                                            
-                                        </div>
-                                    ))}
-                                </li>
-                            </Link>
-                        ))
-                }
-            </ul>
-        </div>
+        <>
+            {!all_tasks ? 
+            
+            (<Spinner />) 
+                :
+            (
+                <div className='day-calendar'>
+                <div className='bg-info' onClick={() => openModalHandler('Create')}><p className='text-center text-dark' style={{cursor: 'pointer', fontWeight: 'bold'}}>Дать поручение</p></div>
+                <ul>
+                    {all_tasks.tasks && all_tasks.tasks.filter(task => task.finish.split('T', 1)[0] >= startDay.format('YYYY-MM-DD') && task.finish.split('T', 1)[0] <= startDay.clone().endOf('day').format('YYYY-MM-DD'))
+                        .map((task) => 
+                            (
+                                <Link to={`task-full-info/${task._id}`}>
+                                    <div className='m-3 bg-warning px-3'>
+                                        {task.content}
+                                        <br />
+                                        <p className="app-text-small d-inline p-3">Выполнить к:</p>
+                                        <p className="d-inline">{new Date(task.finish).toLocaleString('ru').substr(11)}</p>
+                                    </div>
+                                    <li className='day-tasks-list' key={task._id}>
+                                        <p>Исполнители:</p>
+                                        
+                                        {task && task.employee.map((e) => (
+                                           
+                                            <div key={e._id}>
+                                                <span >{e.name}</span>
+                                                <span className='mx-2'>{e.lastName}</span>
+    
+                                            {task && task.read.map((r) => (
+                                                <div className='px-2' key={r._id}>{e._id === r.byEmployee && r.byEmployee !== null && r.ok === true && r.ok !== null ? (<p className='px-2 m-3 bg-info text-dark'>Задание прочитано и принято к исполнению</p>) : null}</div>
+                                            )) } 
+    
+                                            {task && task.completed.map((c) => (
+                                                <div className='px-2' key={c._id}>{e._id === c.byEmployee && c.byEmployee !== null && c.done === true && c.done !== null ? (<p className='px-2 m-3 bg-success text-white'>Задание выполнено</p>) : null}</div>
+                                            )) }
+                                                
+                                            </div>
+                                        ))}
+                                    </li>
+                                </Link>
+                            ))
+                    }
+                </ul>
+            </div>
+            )
+        }
+        </>
     )
 }
 
