30ba2c1d8dda85f4c4f655007b19511eb8a4fb18
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-02-12 16:58:47 +0600

    101

--- src/components/pages/detailed-info/DetailedInfo.js
+++ src/components/pages/detailed-info/DetailedInfo.js
@@ -38,7 +38,7 @@ const DetailedVideo = ({
                             <video className={styles.video_tag} 
                                 src={`http://localhost:5003/${detailed_info.video.filePath}`} autoPlay loop muted>
                             </video>
-                            {console.log(detailed_info.video.filePath)}
+                            {JSON.stringify(detailed_info.video.filePath)}
                             <p className={styles.detailed_info_description}>{detailed_info.video.description}</p>
                             </div>
                             
