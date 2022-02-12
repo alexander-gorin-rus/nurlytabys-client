67f619a2f4ab24d5599c6fbdd57188754e7fab54
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-02-12 17:14:46 +0600

    103

--- src/components/pages/detailed-info/DetailedInfo.js
+++ src/components/pages/detailed-info/DetailedInfo.js
@@ -36,7 +36,7 @@ const DetailedVideo = ({
                         <div className={styles.detailed_info_main}>
                             <div className={styles.detailed_info_video}>
                             <video className={styles.video_tag} 
-                                src={`http://nurlytabys.kz/${detailed_info.video.filePath}`} autoPlay loop muted>
+                                src={`http://localhost:5003/${detailed_info.video.filePath}`} autoPlay loop muted>
                             </video>
                             {JSON.stringify(detailed_info.video.filePath)}
                             <p className={styles.detailed_info_description}>{detailed_info.video.description}</p>
