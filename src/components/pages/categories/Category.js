16bb338e22718bc2e12f60457859cdf58f0cc7fa
alexander-gorin-rus <alexander-gorin-rus@yandex.kz>
2022-03-05 18:46:03 +0600

    05.03.2022/6

--- src/components/pages/categories/Category.js
+++ src/components/pages/categories/Category.js
@@ -41,7 +41,7 @@ const Category = ({
                         : 
                     (
                         <video className={main_styles.main_page_video_video} autoPlay loop muted 
-                            src={`http://nurlytabys.kz/${load_category.category.filePath}`}
+                            src={`${load_category.category.filePath}`}
                         >     
                         </video>
                     )    
